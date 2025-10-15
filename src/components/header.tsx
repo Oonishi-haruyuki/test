66712f17f72ddb99ef2ca4b20dd71f6dabb150ad
2410010 大西玄禮 <2410010@i-seifu.jp>
2025-10-15 00:37:57 +0000

    The above error occurred in the <NotFoundErrorBoundary> component:

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -68,7 +68,7 @@ export function AppHeader() {
             <DropdownMenuContent className="w-56" align="end" forceMount>
               <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">{profile?.name}</p>
+                  <p className="text-sm font-medium leading-none">{profile?.name || user.email}</p>
                   <p className="text-xs leading-none text-muted-foreground">
                     {user.email}
                   </p>
