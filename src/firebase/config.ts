159d0ac369160eebfae4668e2893e65b8fea04c0
2410010 大西玄禮 <2410010@i-seifu.jp>
2025-10-15 00:19:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7743848061-9f621",
+  "appId": "1:944605529409:web:34a5b469a6b2dc4a1acdc0",
+  "apiKey": "AIzaSyD8I0Ez_9b0vChO5hfrBQJ0cGYJlHsFgm4",
+  "authDomain": "studio-7743848061-9f621.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "944605529409"
+};
